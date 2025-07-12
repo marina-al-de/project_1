86c606b3ecd596c9574644e6797bfa5a43d4fe06
marina-al-de <marina07101995@gmail.com>
2025-07-12 22:17:30 +0300

    Update 03 trigger function and trigger for logs.sql

--- part_2/03 trigger function and trigger for logs.sql
+++ part_2/03 trigger function and trigger for logs.sql
@@ -1,38 +1,38 @@
 /*
-Создаём функцию для добавления логов в таблицу logs.changes_in_dm в начале загрузки. 
+Создаём функцию для добавления логов в таблицу logs.logs_for_ds_dm в начале загрузки. 
 */
 
 CREATE OR REPLACE FUNCTION dm.changes_in_dm()
 RETURNS TRIGGER AS
 $func$
 
-DECLARE on_date DATE;
-	account_rk NUMERIC;
+DECLARE p_record_date DATE;
+		p_record_account NUMERIC;
 
 BEGIN 
 
 	 IF TG_OP = 'INSERT' THEN
-       on_date := NEW.on_date;
-	   account_rk := NEW.account_rk;
+      	p_record_date := NEW.on_date;
+	   p_record_account := NEW.account_rk;
     
     ELSIF TG_OP = 'DELETE' THEN
-       on_date := OLD.on_date;
-	   account_rk := OLD.account_rk;
+       p_record_date := OLD.on_date;
+	  p_record_account := OLD.account_rk;
 	END IF;
 		
-    INSERT INTO logs.changes_in_dm (
-        change_tstamp, 
-		dm_changed,                        
+    INSERT INTO logs.logs_for_ds_dm (
+        load_start_tstamp, 
+		destination_table,                        
 		user_name,
-		on_date,
-		account_rk,
+		record_date,
+		record_account,
 		operation
     ) VALUES (
         CURRENT_TIMESTAMP,
         TG_TABLE_NAME,
         current_user,
-		on_date,
-		account_rk,
+		p_record_date,
+		p_record_account,
 		TG_OP
         );
 
@@ -48,6 +48,8 @@ $func$ LANGUAGE plpgsql;
 Создаём триггеры на вставку и удаление записей для dm.dm_account_balance_f и dm.dm_account_turnover_f. 
 */
 
+-- триггеры на старт расчёта
+
 CREATE OR REPLACE TRIGGER changes_dm_account_balance
 BEFORE INSERT OR DELETE ON dm.dm_account_balance_f
 FOR EACH ROW
@@ -58,6 +60,14 @@ BEFORE INSERT OR DELETE ON dm.dm_account_turnover_f
 FOR EACH ROW
 EXECUTE PROCEDURE dm.changes_in_dm();
 
+-- триггеры на окончание расчёта
 
+CREATE OR REPLACE TRIGGER changes_dm_account_balance_end
+AFTER INSERT OR DELETE ON dm.dm_account_balance_f
+FOR EACH ROW
+EXECUTE PROCEDURE ds.update_logs_end();
 
-
+CREATE OR REPLACE TRIGGER changes_dm_account_turnover_end
+AFTER INSERT OR DELETE ON dm.dm_account_turnover_f
+FOR EACH ROW
+EXECUTE PROCEDURE ds.update_logs_end();
