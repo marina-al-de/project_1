e232dd697dd2144512da521c90eb82e789ff2d99
marina-al-de <marina07101995@gmail.com>
2025-07-12 22:16:19 +0300

    Update 02 log table for dm.sql

--- part_2/02 log table for dm.sql
+++ part_2/02 log table for dm.sql
@@ -1,23 +1,12 @@
 /*
-Cоздаём таблицу logs.changes_in_dm со столбцами: 
-- log_id (PRIMARY KEY), 
-- change_tstamp - дата и время внесения изменений в витрину данных,
-- dm_changed - витрина данных, где были изменения, 
-- user_name - пользователь, сделавший изменения, 
-- on_date - дата, за которую производится расчёт в витрине,
-- account_rk - счёт, для которого производится расчёт,
-- operation - insert or delete
+Изменим название таблицы logs.uploads_in_ds и столбцов в ней, чтобы сделать их более универсальными и подходящими для витрин из слоя DM.
 */
 
-CREATE TABLE logs.changes_in_dm
-(
-    log_id SERIAL PRIMARY KEY,         
-    change_tstamp TIMESTAMP NOT NULL, 
-	dm_changed TEXT NOT NULL,                        
-	user_name TEXT,
-	on_date DATE,
-	account_rk NUMERIC,
-	operation TEXT
-);
+
+ALTER TABLE logs.uploads_in_ds RENAME TO logs_for_ds_dm;
+
+
+ALTER TABLE logs.logs_for_ds_dm RENAME COLUMN pk_date TO record_date;
+ALTER TABLE logs.logs_for_ds_dm RENAME COLUMN pk_second_part TO record_account;
 
 
