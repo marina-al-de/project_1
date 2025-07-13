8d5c3c062a95ba3604f98c3b2d87441ef77e1fe7
marina-al-de <marina07101995@gmail.com>
2025-07-13 13:03:07 +0300

    Update 02 log table for dm.sql

--- part_2/02 log table for dm.sql
+++ part_2/02 log table for dm.sql
@@ -1,12 +1,8 @@
 /*
-Изменим название таблицы logs.uploads_in_ds и столбцов в ней, чтобы сделать их более универсальными и подходящими для витрин из слоя DM.
+Изменим название таблицы logs.uploads_in_ds.
 */
 
 
 ALTER TABLE logs.uploads_in_ds RENAME TO logs_for_ds_dm;
-
-
-ALTER TABLE logs.logs_for_ds_dm RENAME COLUMN pk_date TO record_date;
-ALTER TABLE logs.logs_for_ds_dm RENAME COLUMN pk_second_part TO record_account;
 
 
